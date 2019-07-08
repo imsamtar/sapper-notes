c9d9cf2a644a95c995e68a4e11524113eba32f13
SAMEER TARIQ <imsamtar@gmail.com>
2019-07-08 21:30:04 +0500

    Changed about.svelte...

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,18 @@
+<style>
+	#about {
+		max-width: 1024px;
+		margin: auto;
+	}
+	#about > p {
+		text-align: justify;
+	}
+</style>
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
-
-<p>This is the 'about' page. There's not much here.</p>
+<div id="about">
+	<h1>About this site</h1>
+	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem sit rerum similique autem, tempore assumenda dolorum minus, illo laudantium enim modi nulla expedita vitae quibusdam quis doloribus dolor atque error. Animi architecto eveniet assumenda cumque culpa porro natus quas, itaque commodi praesentium tempore fugiat, accusamus ipsa quos amet, odit fuga nulla! Labore numquam iure, saepe, possimus expedita ducimus consectetur provident, nam ut optio reiciendis ullam? Dolorem, maiores eius maxime repellat vero modi nisi repellendus ipsam voluptas, iste impedit. Voluptate explicabo ab minima numquam iste iure voluptatem ratione, quis obcaecati repudiandae quia fugit accusantium a. Repudiandae nesciunt qui quaerat quam voluptatum?</p>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At accusamus nam sequi quam fugiat error culpa laboriosam, reiciendis consequatur? Laborum minus vero aliquid quibusdam similique error unde, illo, nam doloribus iure nihil deleniti beatae, delectus quam ad dolorem consectetur commodi? Eaque deleniti quam earum iste ab, velit voluptas minima blanditiis suscipit assumenda delectus accusantium, pariatur tenetur reprehenderit provident numquam neque mollitia voluptate doloremque quaerat nulla nam? Reprehenderit asperiores minus, voluptatem rem ex laborum minima maxime excepturi eaque commodi, sunt quas natus facilis rerum. Iusto quasi, nemo officiis, officia nihil magnam nobis nisi unde tempore recusandae aspernatur laboriosam aut a, delectus possimus harum quidem molestias deleniti! Magnam cumque deleniti nesciunt quisquam nemo distinctio, soluta mollitia impedit nihil, quo similique in neque dolorum, rem voluptates vel rerum? Animi quia impedit porro laboriosam voluptates. Corrupti, praesentium. Sed voluptas cumque eligendi facere et libero ratione atque quibusdam sunt blanditiis? Voluptate hic totam, necessitatibus modi iusto at! Rem, saepe perspiciatis? Ipsa, velit delectus. Eveniet hic qui nemo ratione magni itaque ullam culpa deserunt excepturi, minus magnam quo? Dolorem consequatur ducimus cupiditate dignissimos accusamus libero similique, eius dolore ipsum incidunt, ad id. Magni autem facilis, incidunt aliquam deleniti unde necessitatibus eveniet nisi numquam placeat molestias reprehenderit!</p>
+</div>

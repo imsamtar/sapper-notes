ed7ae6e0562f485a8227fc184b6169c5947393fa
SAMEER TARIQ <imsamtar@gmail.com>
2019-07-10 10:30:56 +0500

    Changed links style in auth/index.svelte...

--- src/routes/auth/index.svelte
+++ src/routes/auth/index.svelte
@@ -4,18 +4,29 @@
 </script>
 <style>
 div {
-    height: 50vmin;;
+    --h: 15rem;
+    height: var(--h);
+    width: calc(2 * var(--h));
+    margin: 0 auto;
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-gap: 10px;
 }
 div > a {
+    border-radius: 50%;
     font-size: 3rem;
     font-weight: 700;
     mix-blend-mode: exclusion;
     background: white;
     display: grid;
     place-content: center;
+}
+@media (max-width: 768px){
+    div {
+        height: calc(2 * var(--h));
+        width: var(--h);
+        grid-template-columns: 1fr;
+    }
 }
 </style>
 <div>
